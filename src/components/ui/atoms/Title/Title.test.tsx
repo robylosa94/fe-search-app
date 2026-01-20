521e894699fb9b80d4afa55590c62054eb69ace4
robylosa94 <roby94.losa@tiscali.it>
2026-01-20 21:11:43 +0100

    fix: update test titles

--- src/components/ui/atoms/Title/Title.test.tsx
+++ src/components/ui/atoms/Title/Title.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 import Title from "./Title";
 
 describe("Title", () => {
-  test("rende un h2 di default", () => {
+  test("renderizza un titolo h2 di default", () => {
     render(<Title text="Test Title" />);
     const titleElement = screen.getByText("Test Title");
     expect(titleElement.tagName).toBe("H2");
   });
 
-  test("rende un h3 se specificato", () => {
+  test("renderizza un titolo h3", () => {
     render(<Title text="Test H3" as="h3" />);
     const titleElement = screen.getByText("Test H3");
     expect(titleElement.tagName).toBe("H3");
