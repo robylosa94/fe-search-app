26fe8e2cdeec066b6eadc56261ac432a3fc939de
robylosa94 <roby94.losa@tiscali.it>
2026-01-20 21:38:54 +0100

    fix: customize 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,16 @@
-import { Container } from "@/components/ui";
+import { Container, Title } from "@/components/ui";
+import s from "./NotFound.module.css";
 
 export default function NotFound() {
   return (
-    <section>
+    <main className={s.notFound}>
       <Container>
-        <h1>Error 404 - Page not found</h1>
+        <div className={s.notFound__image}>
+          <img src="/lost.gif" alt="Man lost" width={198} height={187} />
+        </div>
+        <h1 className={s.notFound__title}>Error 404</h1>
+        <Title as="h2" text="The page you are looking for does not exist." />
       </Container>
-    </section>
+    </main>
   );
 }
